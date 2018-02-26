49616b56e547dd5c1f95638a29f6522ba11fb7a6
Kevin Fagan <kevinfagan608@gmail.com>
2018-02-25 20:11:06 -0800

    Added switch button in actionbar

--- app/src/main/java/com/pedigreetechnologies/diagnosticview/ViewPagerFragmentActivity.java
+++ app/src/main/java/com/pedigreetechnologies/diagnosticview/ViewPagerFragmentActivity.java
@@ -85,9 +85,9 @@ public class ViewPagerFragmentActivity extends AppCompatActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
+        //if (id == R.id.action_settings) {
+        //    return true;
+        //}
 
         return super.onOptionsItemSelected(item);
     }
