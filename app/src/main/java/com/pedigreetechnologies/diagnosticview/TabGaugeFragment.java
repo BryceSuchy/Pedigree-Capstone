0c86513a49af1968da1cc7f7e01d96e51f02d88a
Kevin Fagan <kevinfagan608@gmail.com>
2018-03-10 20:31:37 -0800

    Gauges are working properly

--- app/src/main/java/com/pedigreetechnologies/diagnosticview/TabGaugeFragment.java
+++ app/src/main/java/com/pedigreetechnologies/diagnosticview/TabGaugeFragment.java
@@ -1,12 +1,9 @@
 package com.pedigreetechnologies.diagnosticview;
 
-import android.app.Activity;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
-import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
-import android.util.AttributeSet;
 import android.util.DisplayMetrics;
 import android.view.Gravity;
 import android.view.LayoutInflater;
@@ -16,9 +13,6 @@ import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.ScrollView;
 import android.widget.TextView;
-
-import org.xmlpull.v1.XmlPullParser;
-
 import java.util.ArrayList;
 import de.nitri.gauge.Gauge;
 
@@ -34,10 +28,11 @@ public class TabGaugeFragment extends Fragment {
         }
 
         final ScrollView scrollView = (ScrollView)inflater.inflate(R.layout.tab_gauge_layout, container, false);
+        RelativeLayout relativeLayout = scrollView.getRootView().findViewById(R.id.gaugeholder);
 
         //Create two layouts both taking up half the screen to have 2 views next to each other
         LinearLayout gaugeLayoutLeft = scrollView.findViewById(R.id.gaugeLayout1);
-        LinearLayout gaugeLayoutRight = scrollView.findViewById(R.id.gaugeLayout2);
+        //LinearLayout gaugeLayoutRight = scrollView.findViewById(R.id.gaugeLayout2);
 
         Bundle extras = getArguments();
         if (extras != null) {
@@ -56,56 +51,41 @@ public class TabGaugeFragment extends Fragment {
             //If there is not a min and a max create a text view
             if(Double.isNaN(tempParm.getMin())|| Double.isNaN(tempParm.getMax())){
                 TextView textView = new TextView(this.getContext());
-                textView.setLayoutParams(new ViewGroup.LayoutParams(width / 2, (int)((width / 2))));
+                textView.setLayoutParams(new ViewGroup.LayoutParams((int) (width / 2), (int)((width / 2))));
                 textView.setText("0.0 " + tempParm.getUnits() + "\n" + tempParm.getLabel());
                 textView.setMinLines(2);
                 textView.setGravity(Gravity.CENTER);
                 view = textView;
             }
+
             //If there is a min and a max create a gauge view
             else {
 
-                //view = inflater.inflate(R.layout.gauge_draw, containter, false);
-                //Gauge gauge = getView().findViewById(R.id.gauge1);
-                //Gauge gauge = new Gauge(this.getContext());
-                //scrollView.removeAllViews();
-                RelativeLayout relativeLayout = scrollView.getRootView().findViewById(R.id.gaugeholder);
-                Gauge gauge = relativeLayout.findViewById(R.id.gauge1);
-                //Gauge gauge = (Gauge) getView().findViewById(R.id.gauge1);
-                if (gauge != null) {
-                    gauge.setLayoutParams(new ViewGroup.LayoutParams(width / 2, (int)((width/2))));
-                    gauge.setValue(0);
-                    gauge.invalidate();
-                    view = gauge;
-                }
-
-
-                /*GaugeView gaugeView = new GaugeView(this.getContext());
-                gaugeView.setLayoutParams(new ViewGroup.LayoutParams(width / 2, (int)((width / 2) * .8f)));
-                gaugeView.setGaugeMin((float) tempParm.getMin());
-                gaugeView.setGaugeMax((float) tempParm.getMax());
-                gaugeView.setGaugeValue(0);
-                gaugeView.setGaugeUnits(tempParm.getUnits());
-                gaugeView.setLabel(tempParm.getLabel());
-                gaugeView.invalidate();
-                view = gaugeView;*/
-
+                String gaugeID = tempParm.getLabel().replaceAll("\\s+","");
+                int resID = getResources().getIdentifier(gaugeID, "id", getActivity().getPackageName());
+                Gauge gauge = relativeLayout.findViewById(resID);
+                gauge.setVisibility(Gauge.VISIBLE);
+                RelativeLayout.LayoutParams layoutParams = new RelativeLayout.LayoutParams((int) (width / 1.7), (int) (width / 1.7));
+                gauge.setLayoutParams(layoutParams);
+                gauge.setValue(0);
+                gauge.invalidate();
+                view = gauge;
             }
 
             //Set a tag so you know which view is attached to what label
             view.setTag(tempParm.getLabel());
 
             //Add the views to the parents left to right
-            if(i % 2 == 0){
+            //if(i % 2 == 0){
                 if(view.getParent()!=null)
                     ((ViewGroup)view.getParent()).removeView(view); // <- fix
                 gaugeLayoutLeft.addView(view);
-            }
-            else{
+           // }
+            /*else{
                 if(view.getParent()!=null)
                     ((ViewGroup)view.getParent()).removeView(view); // <- fix
                 gaugeLayoutRight.addView(view);
-            }
+            }*/
         }
 
         //Handler to update the gauges with the most recent values
@@ -129,8 +109,10 @@ public class TabGaugeFragment extends Fragment {
                         }
                     }
                 }
-                else if(view instanceof GaugeView){
-                    ((GaugeView)view).updateAndRefreshValue(Float.parseFloat(value));
+                else if(view instanceof Gauge){
+                    //((GaugeView)view).updateAndRefreshValue(Float.parseFloat(value));
+                    ((Gauge)view).setLowerText(value);
+                    ((Gauge)view).moveToValue(Float.parseFloat(value));
                 }
             }
         };
